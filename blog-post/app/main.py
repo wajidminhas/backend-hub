8b71ed660622b4e1bccf1988d7a2649561996216
wajidminhas <shanitent667@gmail.com>
2025-04-15 19:47:36 +0500

    main page edited

---
+++ blog-post/app/main.py
@@ -0,0 +1,16 @@
+# app/main.py
+from fastapi import FastAPI
+from app.database import create_db_and_tables
+from app.routes.post import router as post_router
+from contextlib import asynccontextmanager
+app = FastAPI(title="Blog API")
+
+@asynccontextmanager("lifespan")
+def on_startup():
+    create_db_and_tables()
+
+app.include_router(post_router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Blog API"}
