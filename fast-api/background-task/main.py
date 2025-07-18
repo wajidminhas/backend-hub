065d1e35e494e31d85eb486c1c5913c3b287a7a5
wajidminhas <shanitent667@gmail.com>
2025-07-18 19:10:19 +0500

    basic practic of api

---
+++ fast-api/background-task/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, BackgroundTasks
+from pydantic import EmailStr
+
+app: FastAPI = FastAPI()
+
+
+@app.get('/')
+async def root():
+    return {"message": "Welcome to the Notification Service!"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id, "message": "This is your item!"}
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int):
+#     return {"item_id": item_id}
+
+
+async def write_notification(email : EmailStr, message : str = 'You have a new notification!'):
+    print(f"Notification sent to {email}: {message}")
+    with open('log.txt', mode= "w") as email_file:
+        email_file.write(f"{email}\n{message}\n")
+
+@app.post("/send-notification/{email}")
+async def send_notification(email: EmailStr, background_tasks: BackgroundTasks):
+    """
+    Endpoint to send a notification to a user.
+    """
+    background_tasks.add_task(write_notification, email)
+    return {"message": "Notification sent in the background!"}
